6aefd2f544ad547c90f420949e1f584cc52861df
Guido Naselli <guidonaselli@gmail.com>
2023-12-22 16:48:19 -0300

    html-to-text fix intent

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     domains: ["images.unsplash.com"],
   },
   assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
