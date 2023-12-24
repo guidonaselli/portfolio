69ffbbaa220050da953cdb2db8cdcd7bf1288f3f
Guido Naselli <guidonaselli@gmail.com>
2023-12-24 20:11:41 -0300

    Refactor sendEmail function and update email sending parameters

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,22 +1,17 @@
 import { Resend } from "resend";
-import ReactDOMServer from "react-dom/server"; // Use renderToString instead
 import { EmailTemplate } from "@/email/email-template";
-import * as React from "react";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { data } from "autoprefixer";
-import { m } from "framer-motion";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export const sendEmail = async (formData: FormData) => {
+export const sendEmail = (formData: FormData) => {
   let senderEmail = formData.get("senderEmail") as string;
   const message = formData.get("message") as string;
 
   try {
     // Enviar el correo electrónico
-    const data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: ["guidonaselli@gmail.com"],
+    const data = resend.emails.send({
+      from: "<onboarding@resend.dev>",
+      to: "guidonaselli@gmail.com",
       subject: "Message from portfolio contact form",
       reply_to: senderEmail,
       text: message,
