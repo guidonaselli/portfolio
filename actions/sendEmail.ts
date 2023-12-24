67990a62a82d000c9fdd6b9ef5da735d9e391b53
Guido Naselli <guidonaselli@gmail.com>
2023-12-24 20:00:52 -0300

    Refactor sendEmail function and update email recipient

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -8,38 +8,20 @@ import { m } from "framer-motion";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-function validateEmail(email: string) {
-  var re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-function validateString(input: string, maxLength: number) {
-  return typeof input === "string" && input.length <= maxLength;
-}
-
 export const sendEmail = async (formData: FormData) => {
   let senderEmail = formData.get("senderEmail") as string;
   const message = formData.get("message") as string;
-
-  if (!validateEmail(senderEmail)) {
-    throw new Error("Invalid sender email");
-  }
-  if (!validateString(message, 5000)) {
-    throw new Error("Invalid message");
-  }
 
   try {
     // Enviar el correo electrónico
     const data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "guidonaselli@gmail.com",
-      subject: "Message from contact form",
+      to: ["guidonaselli@gmail.com"],
+      subject: "Message from portfolio contact form",
       reply_to: senderEmail,
       text: message,
       react: EmailTemplate({ message, senderEmail }), // <EmailTemplate message={message} senderEmail={senderEmail} />,
     });
-
     return { data };
   } catch (error) {
     return {
